9fff2f5b5a9da2893a8a2b8f41da80b3c0c2a403
marb <marb61a@gmail.com>
2015-11-17 08:58:23 +0000

    added modal service

--- www/js/services.js
+++ www/js/services.js
@@ -11,3 +11,42 @@ angular.module('stockMarketApp.services', [])
     }
   };
 })
+
+.service('modalService', function($ionicModal){
+  this.openModal = function(){
+    var _this = this;
+
+    if (id == 1) {
+      // Create the search modal
+      $ionicModal.fromTemplateUrl('templates/search.html', {
+        scope: null,
+        controller: 'SearchCtrl'
+      }).then(function(modal) {
+        _this.modal = modal;
+        _this.modal.show();
+      });
+    } else if (id == 2) {
+      $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+      }).then(function(modal) {
+        $scope.modal = modal;
+      });
+    } else if (id == 3) {
+      $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+      }).then(function(modal) {
+        $scope.modal = modal;
+      });
+    }
+  };
+  
+  this.closeModal = function() {
+
+    var _this = this;
+
+    if (!_this.modal) return;
+    _this.modal.hide();
+    _this.modal.remove();
+  };
+  
+})
