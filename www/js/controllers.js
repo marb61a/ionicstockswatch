ba0f12000db21b1fe485256c89f1f2b5240281a9
marb <marb61a@gmail.com>
2015-11-20 09:15:18 +0000

    updated stock control controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -72,5 +72,44 @@ angular.module('stockMarketApp.controllers', [])
                     $scope.stockNotes = notesService.getNotes($scope.ticker);
                 });
             };
+            
+              $scope.openNote = function(index, title, body) {
+              $scope.note = {title: title, body: body, date: $scope.todayDate, ticker: $scope.ticker};
+        
+              var note = $ionicPopup.show({
+                template: '<input type="text" ng-model="note.title" id="stock-note-title"><textarea type="text" ng-model="note.body" id="stock-note-body"></textarea>',
+                title: $scope.note.title,
+                scope: $scope,
+                buttons: [
+                  {
+                    text: 'Delete',
+                    type: 'button-assertive button-small',
+                    onTap: function(e) {
+                      notesService.deleteNote($scope.ticker, index);
+                    }
+                  },
+                  {
+                    text: 'Cancel',
+                    type: 'button-small',
+                    onTap: function(e) {
+                      return;
+                    }
+                  },
+                  {
+                    text: '<b>Save</b>',
+                    type: 'button-balanced button-small',
+                    onTap: function(e) {
+                      notesService.deleteNote($scope.ticker, index);
+                      notesService.addNote($scope.ticker, $scope.note);
+                      console.log("save: ", $scope.note);
+                    }
+                  }
+                ]
+              });
+              note.then(function(res) {
+                $scope.stockNotes = notesService.getNotes($scope.ticker);
+              });
+            };
+
 
       }])
