940c5b16cbd18d2cac817fdd28b2335cc5bd8e64
marb <marb61a@gmail.com>
2015-11-30 09:08:14 +0000

    added news service

--- www/js/services.js
+++ www/js/services.js
@@ -195,3 +195,32 @@ angular.module('stockMarketApp.services', [])
   };
   
 })
+
+
+.factory('newsService', function($q, $http) {
+
+  return {
+
+    getNews: function(ticker) {
+      var deferred = $q.defer(),
+
+        x2js = new X2JS(),
+
+        url = "http://finance.yahoo.com/rss/headline?s=" + ticker;
+
+      $http.get(url)
+        .success(function(xml) {
+          var xmlDoc = x2js.parseXmlString(xml),
+            json = x2js.xml2json(xmlDoc),
+            jsonData = json.rss.channel.item;
+          deferred.resolve(jsonData);
+        })
+        .error(function(error) {
+          deferred.reject();
+          console.log("News error: " + error);
+        });
+
+      return deferred.promise;
+    }
+  };
+})
