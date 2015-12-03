21f314645c559c84bbffa8efdf98f1b75dca131c
marb <marb61a@gmail.com>
2015-12-03 09:02:08 +0000

    added follow stock service

--- www/js/services.js
+++ www/js/services.js
@@ -104,6 +104,39 @@ angular.module('stockMarketApp.services', [])
   return notesCache;
 })
 
+.factory('followStockService', function(myStocksArrayService, myStocksCacheService){
+  return{
+    follow : function(ticker){
+      var stockToAdd = {"ticker" : ticker};
+      
+      myStocksArrayService.push(stockToAdd);
+      myStocksCacheService.put('myStocks', myStocksArrayService);
+    },
+    
+    unfollow : function(ticker){
+      for(var i = 0; i < myStocksArrayService.length; i++){
+        if(myStocksArrayService[i].ticker == ticker){
+          myStocksArrayService.splice(i, 1);
+          myStocksCacheService.remove('myStocks');
+          myStocksCacheService.put('myStocks', myStocksArrayService);
+
+          break;
+        }
+      }
+    },
+    
+    checkFollowing : function(ticker){
+      for (var i = 0; i < myStocksArrayService.length; i++) {
+        if (myStocksArrayService[i].ticker == ticker) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+  };
+})
+
 .factory('chartDataService', function($q, $http, encodeURIService, chartDataCacheService){
   var getHistoricalData = function(ticker, fromDate, todayDate){
     var deferred = $q.defer(),
