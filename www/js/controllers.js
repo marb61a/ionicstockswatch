9f02c4943f8792eebb3080ccfcd6b926e1ff74c5
marb <marb61a@gmail.com>
2015-12-09 09:09:26 +0000

    added search controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -157,3 +157,27 @@ angular.module('stockMarketApp.controllers', [])
          var marginBottom = ($window.innerWidth / 100) * 10;
 
       }])
+      
+.controller('SearchCtrl', ['$scope', '$state', 'modalService', 'searchService', function($scope, $state, modalService, searchService){
+  $scope.closeModal = function(){
+    modalService.closeModal();
+  };
+  
+  $scope.search = function(){
+    $scope.searchResults = '';
+    startSearch($scope.searchQuery);
+  }
+  
+  var startSearch = ionic.debounce(function(query) {
+      searchService.search(query)
+        .then(function(data) {
+          $scope.searchResults = data;
+        });
+    }, 400);
+
+  $scope.goToStock = function(ticker) {
+    modalService.closeModal();
+    $state.go('app.stock', {stockTicker: ticker});
+  };
+  
+}])      
